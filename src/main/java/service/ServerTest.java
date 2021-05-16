e79806b0715187b3e28e1177a24a7ebc0238893b
dominic.eckerle@mnd.thm.de <9UyjWT8qu9Sn>
2021-05-16 12:15:30 +0200

    Added Test.java and ServerTest.java to eckerle branch. Osm and gtfs data and the already built graph of friedberg are in the resources directory.

---
+++ src/main/java/service/ServerTest.java
@@ -0,0 +1,8 @@
+package service;
+
+public class ServerTest {
+
+    public static void main(String[] args) {
+        OTPFacade.startOTPServer("2");
+    }
+}
---
+++ src/main/java/service/Test.java
@@ -0,0 +1,22 @@
+package service;
+
+import model.Routemodel.Actions;
+import model.Routemodel.Location;
+import java.time.LocalDateTime;
+
+public class Test {
+
+    public static void main(String[] args) {
+        runFriedberg();
+    }
+
+    public static void runFriedberg () {
+        //OTPFacade.buildGraph("friedberg.pbf", "gtfs_friedberg.zip", "1");
+        OTPFacade otp = new OTPFacade("Europe/Berlin", 2021, 4, 30, 10, 0);
+        Location from = new Location(50.43280, 8.66358);
+        Location to = new Location(50.33108, 8.75919);
+        LocalDateTime queryTime = LocalDateTime.of(2021, 4, 30, 10, 0);
+        otp.createSimpleRoute(from, to, queryTime,1, Actions.JSONOBEJCT_AS_JSON);
+    }
+#
+}
